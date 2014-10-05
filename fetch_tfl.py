95b0a8ca5d56406b6c6dfd74eaa3f17a10c732be
Minh-Long Do <minhlong94@yahoo.de>
2014-10-05 23:13:18 +0100

    Encapsulate tfl data fetch functionality into a function.

--- fetch_tfl.py
+++ fetch_tfl.py
@@ -1,8 +1,15 @@
 import httplib
 httplib.HTTPConnection.debuglevel = 1
 import urllib2
-request = urllib2.Request('http://www.tfl.gov.uk/tfl/syndication/feeds/cycle-hire/livecyclehireupdates.xml')
-opener = urllib2.build_opener()
-feeddata = opener.open(request).read()
 
-print str(len(feeddata))
+## Fetch TFL data
+#
+# Return: Tuple of string of TFL feed data and length of feed data
+#
+def fetch_data(url='http://www.tfl.gov.uk/tfl/syndication/feeds/cycle-hire/livecyclehireupdates.xml'):
+  request = urllib2.Request(url)
+  opener = urllib2.build_opener()
+  feeddata = opener.open(request).read()
+  print feeddata
+  print str(len(feeddata))
+  return (feeddata, len(feeddata))
