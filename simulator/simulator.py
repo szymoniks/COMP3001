f8228e4201d2e1338f64a999f79b22298d81cf18
Dominik Krejcik <dominik.krejcik@gmail.com>
2014-11-22 17:35:16 +0000

    Implement simulator run function

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -9,9 +9,17 @@ class Simulator:
         self._create_station_index(self.stations)
         self.time = 0  # Minutes of the simulation day
 
-    def run(self, time_step=1):
+    def run(self, algorithm, time_step=1):
         self.time_step = time_step
-        pass
+        for i in xrange(0, 60*24, time_step):
+            self.time = i
+            starting_trips = self.trips_starting_now()
+            ending_trips = self.trips_ending_now()
+            for trip in ending_trips:
+                self._end_trip(trip)
+            for trip in starting_trips:
+                self._start_trip(trip)
+            algorithm.update(self)
 
     def get_station(self, station_id):
         return self.stations_index[station_id]
@@ -37,7 +45,7 @@ class Simulator:
         return filter(f, self.trips)
 
     def trips_ending_now(self):
-        f = lambda t: self._within_time_step_past(t.start_minute_of_day)
+        f = lambda t: self._within_time_step_past(t.end_minute_of_day)
         return filter(f, self.trips)
 
     def active_trips(self):
@@ -54,3 +62,16 @@ class Simulator:
 
     def _within_time_step_past(self, time):
         return time < self.time and abs(time - self.time) <= self.time_step
+
+    # TODO: Handle semi-successful trips
+    def _end_trip(self, trip):
+        if self.get_station(trip.end_id).add_bikes(1):
+            trip.status = TripStatus.SUCCESSFUL
+        else:
+            trip.status = TripStatus.FAILED
+
+    def _start_trip(self, trip):
+        if self.get_station(trip.start_id).remove_bikes(1):
+            trip.status = TripStatus.ACTIVE
+        else:
+            trip.status = TripStatus.FAILED
