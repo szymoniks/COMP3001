6be75f7361b949d1c22d4690361b38f67358bfc3
Kelvin Khoo <gp01kkky@hotmail.com>
2014-11-25 14:59:22 +0000

    Modified retrieve weather function

--- simulator/retrieve_weather.py
+++ simulator/retrieve_weather.py
@@ -9,15 +9,21 @@ event = "Events"
 weather_arr = []
 firstLine = False
 
-#main function
-with open('whistory2013-14.csv','rb') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    for row in csvfile:
-        if firstLine == False:
-            firstLine = True
-            continue
+#Get weather data
+def loadWeatherData(filepath)
+    with open('data/whistory2013-14.csv','rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in csvfile:
+            if firstLine == False:
+                firstLine = True
+                continue
 
-        bst, max_tempc, mean_tempc, min_tempc, max_humid, mean_humid, min_humid, max_visibility, mean_visibility, min_visibility, max_wind_speed, mean_wind_speed, max_gust_speed, precipitation, cloudcover, events, wind_degree = row.split(",")
-        weather = Weather(bst, max_tempc, mean_tempc, min_tempc, max_humid, mean_humid, min_humid, max_visibility, mean_visibility, min_visibility, max_wind_speed, mean_wind_speed, max_gust_speed, precipitation, cloudcover, events, wind_degree)
-        weather_arr.append(weather)
+            bst, max_tempc, mean_tempc, min_tempc, max_humid, mean_humid, min_humid, max_visibility, mean_visibility, min_visibility, max_wind_speed, mean_wind_speed, max_gust_speed, precipitation, cloudcover, events, wind_degree = row.split(",")
+            weather = Weather(bst, max_tempc, mean_tempc, min_tempc, max_humid, mean_humid, min_humid, max_visibility, mean_visibility, min_visibility, max_wind_speed, mean_wind_speed, max_gust_speed, precipitation, cloudcover, events, wind_degree)
+            weather_arr.append(weather)
+
+
+#main function
+filepath = "data/whistory2013-14.csv"
+loadWeatherData(filepath)
 
