ce11ebcee00de12287f228c1887682dc13d68b86
Szymon Makula <szymoniks@hotmail.com>
2014-11-25 14:42:40 +0000

    Main function

--- simulator/main.py
+++ simulator/main.py
@@ -1,5 +1,16 @@
+import trip_util
+import load_station
+import retrieve_weather
+from simulator import Simulator
+
 def main():
-	pass
+	trips = load_trips("fucking file")
+	weather = load_wather("fucking file")
+	stations = load_station("fucking file")
+
+	simulator = Simulator(trips, stations)
+
+	
 
 if __name__ == "__main__":
     main()
