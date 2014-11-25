0a1a46d318530a4a1e0609a59f813e520098007a
Szymon Makula <szymoniks@hotmail.com>
2014-11-25 15:37:06 +0000

    Main function

--- simulator/main.py
+++ simulator/main.py
@@ -4,13 +4,16 @@ import retrieve_weather
 from simulator import Simulator
 
 def main():
-	trips = load_trips("fucking file")
+	trips = []
+	load_trips("fucking file")
 	stations = load_station("fucking file")
 	weather = load_wather("data/whistory2013-14.csv")
 
 	simulator = Simulator(trips, stations)
 
-	testAlgorithm = Test
+	testAlg = TestAlg()
+
+	simulator.l
 
 	
 
