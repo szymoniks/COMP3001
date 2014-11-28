6365cc74f3e8ee0fc67c3698c42653968e56bd95
Kelvin Khoo <gp01kkky@hotmail.com>
2014-11-28 17:02:00 +0000

    Please fix the csv date bug

--- simulator/weather.py
+++ simulator/weather.py
@@ -1,9 +1,22 @@
 import xml.etree.ElementTree as ET
+from datetime import datetime as dt
+
+#convert date in string format to datetime format
+#arg0: date time string
+def convertDate(string):
+    try:
+        condate = dt.strptime(string, "%d/%m/%Y")
+
+    except ValueError:
+        condate = dt.strptime(string, "%d/%m/%Y")
+
+    return condate
+
 
 class Weather:
     
     def __init__(self, date,max_tempc,mean_tempc,min_tempc,max_humid,mean_humid,min_humid,max_visibility,mean_visibility,min_visibility,max_wind_speed,mean_wind_speed,max_gust_speed,precipitation,cloudcover,events,wind_degree):
-        self.date = date
+        self.date = convertDate(date)
         self.max_tempc = max_tempc
         self.mean_tempc = mean_tempc
         self.min_tempc = min_tempc
