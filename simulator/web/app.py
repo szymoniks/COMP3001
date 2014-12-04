68298944ba8e48b5943ac4a6d6380f820c17cf5d
Dominik Krejcik <dominik.krejcik@gmail.com>
2014-12-04 13:22:17 +0000

    Code uploads

--- simulator/web/app.py
+++ simulator/web/app.py
@@ -12,7 +12,15 @@ class upload:
         return render.index()
 
     def POST(self):
-        return None
+        code = web.input().get("algorithm", "")
+        write_algorithm(code)
+        return "Cool bro"
+
+
+def write_algorithm(code):
+    file = open("uploads/algorithm.py", "wb")
+    file.write(code)
+    file.close()
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
