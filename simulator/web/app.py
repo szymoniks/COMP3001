e604335aff0c791f77007bcdaf31d15208ae6bf8
Szymon Makula <szymoniks@hotmail.com>
2014-12-04 13:35:39 +0000

    Main method generation for web app

--- simulator/web/app.py
+++ simulator/web/app.py
@@ -22,6 +22,38 @@ def write_algorithm(code):
     file.write(code)
     file.close()
 
+    main_file = open("uploads/main.py", "w")
+    file_content = 'import ' + className + '''
+import trip_util
+import load_station
+import retrieve_weather
+from simulator import Simulator
+from basic import BasicAlg
+from rush import RushAlg
+
+import basic
+
+def main():
+    trips = []
+    print "Loading trips..."
+    trips = trip_util.load_trips("data/trips/sampleTrips.csv")
+    print "Loading stations..."
+    stations = load_station.load_stations("data/stations.xml")
+    print "Loading weather..."
+    weather = retrieve_weather.load_weather("data/whistory2013-14.csv")
+
+    simulator = Simulator(trips, stations, weather)
+    ''' + varName + ' = ' + className + '''()
+
+    simulator.run(''' + varName + ', "data.xml", ' + interval + ''')
+
+if __name__ == "__main__":
+    main()'''
+
+    main_file.write(file_content)
+    main_file.close()
+
+
 if __name__ == "__main__":
     app = web.application(urls, globals())
     app.run()
